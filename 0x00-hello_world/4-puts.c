ad52c16d2bdef0d532426fdb8d80038cdd953d44
Abayomi <absantec@example.com>
2023-07-13 19:46:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include > stdio.h
+/**
+ * main - A programme that prints a line as puts function
+ * Return: 0 (success)
+ */
+int main(void)
+	/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+ 

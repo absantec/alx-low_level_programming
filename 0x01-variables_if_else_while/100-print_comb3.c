dd79d79f4af3157e637c3a1730b9be8e38d85851
Abayomi <absantec@example.com>
2023-08-16 00:57:43 -0700

    dog.h

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,6 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	int hundreds = '0';
 
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
@@ -15,13 +14,11 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens > ones) || (hundreds > tens)))
 				{
-					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					if (!(ones == '9' && tens == '8'))
 					{
 						putchar(',');
 						putchar(' ');
---
+++ README.md
@@ -0,0 +1,9 @@
+0x0E. C - Structures, typedef
+Define a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
+
+1. A dog is the only thing on earth that loves you more than you love yourself
+2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

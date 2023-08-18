8160aa8954d1302ba35a1b48e1715e64d218fe4f
Abayomi <absantec@example.com>
2023-08-18 04:30:27 -0700

    100-main_opcdes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints its own opcodes
@@ -33,7 +34,7 @@ int main(int  argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		print("%02hhx", arr[i])
+		print("%02hhx", arr[i]);
 	}
 	return (0);
 }

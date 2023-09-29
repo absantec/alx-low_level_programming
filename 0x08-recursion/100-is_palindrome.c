e5c115ccbc7a142fb8969d7819f2c3b1f07443ef
absantec <absantec@gmail.com>
2023-09-29 02:25:23 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,9 +13,8 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_recursion(S)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
-
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of

b94e4fa0bd754b93bb1541688da079c5fee59a1a
absantec <absantec@gmail.com>
2023-10-25 02:08:15 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/** main - print the number of arguments passed to the program
+/**
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments to be passed
  * @argv: array of arguments passed
  *

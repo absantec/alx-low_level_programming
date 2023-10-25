7410c536bb30be7d89a2856736e61506a13b0030
absantec <absantec@gmail.com>
2023-10-24 20:33:35 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-/**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-int _strlen(char *string)
-{
-        int i;
 
-        for (i = 0; string[i] != '\0'; i++)
-                ;
-        return (i);
-}
 /**
- * string_nconcat - s1 and n bytes of s2; return ptr to string
+ * string_nconcat - concatenates string
  * @s1: string 1
  * @s2: string 2
- * @n: n bytes to concat from string 2
- * Return: pointer to concatenated string
+ * @n: number of bytes to concatenates from s2
+ * Return: concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *ptr;
-        int num, len, i, j;
+	unsigned int i;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
+	char *output;
 
-        num = n;
-        if (s1 == NULL) /* account for NULL strings */
-  s1 = "";
-        if  (s2 == NULL)
-                s2 = "";
-        if (num <= 0) /* accunt for negative n bytes */
-                return (NULL);
-        if (num >= _strlen(s1)) /* account for n too big */
-                num = _strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
 
-        len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
-
-        ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-        if (ptr == NULL)
-                return (NULL);
-
-        for (i = 0; s1[i] != '\0'; i++) /* concat */
-                ptr[i] = s1[i];
-        for (j = 0; j < num; j++)
-                ptr[i + j] = s2[j];
-        ptr[i + j] = '\0';
-
-        return (ptr);
+	output = malloc(sizeof(char) * (s1len + n) + 1);
+	if (output == NULL)
+		return (NULL);
+	if (n >= s2len)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; s2[i] != '\0'; i++)
+			output[s1len + i] = s2[i];
+		output[s1len + i] = '\0';
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; i < n; i++)
+			output[s1len + 1] = s2[i];
+		output[s1len + i] = '\0';
+	}
+	return (output);
 }

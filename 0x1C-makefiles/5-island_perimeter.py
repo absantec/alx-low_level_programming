befbb213c5d533296463971e8da7ec75f18e55d6
absantec <absantec@gmail.com>
2024-02-26 08:01:47 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,57 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+'''
+Create a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid:
+'''
+
+
+def neighbors(grid, up, down, row_neighbors, col_neighbors):
+    '''
+    Return number of
+    sides of the lake
+    '''
+    num_of_sides = 0
+
+    '''
+    up
+    '''
+    if up > 0 and grid[up - 1][down]:
+        num_of_sides += 1
+
+    '''
+    down
+    '''
+    if up < row_neighbors - 1 and grid[up + 1][down]:
+        num_of_sides += 1
+
+    '''
+    left
+    '''
+    if down > 0 and grid[up][down - 1]:
+        num_of_sides += 1
+
+    '''
+    right
+    '''
+    if down < col_neighbors - 1 and grid[up][down + 1]:
+        num_of_sides += 1
+
+    return num_of_sides
 
 
 def island_perimeter(grid):
-        """Returns the perimeter of the island described in grid"""
-            c = 0
-                length = len(grid) - 1
-                    width = len(grid[0]) - 1
+    '''
+    Return perimiter of
+    lake of ones inside
+    lake of zeros
+    '''
+    perimiter_count = 0
+    row_length = len(grid)
+    col_length = len(grid[0])
 
-                        for i, r in enumerate(grid):
-                                    for j, n in enumerate(r):
-                                                    if n == 1:
-                                                                        if i == 0 or grid[i - 1][j] != 1:
-                                                                                                c += 1
-                                                                                                                if j == 0 or grid[i][j - 1] != 1:
-                                                                                                                                        c += 1
-                                                                                                                                                        if j == width or grid[i][j + 1] != 1:
-                                                                                                                                                                                c += 1
-                                                                                                                                                                                                if i == length or grid[i + 1][j] != 1:
-                                                                                                                                                                                                                        c += 1
-                                                                                                                                                                                                                            return c
+    for row in range(0, row_length):
+        for col in range(0, col_length):
+            if grid[row][col]:
+                perimiter_count += (4 - neighbors(grid, row, col,
+                                                  row_length, col_length))
+    return perimiter_count

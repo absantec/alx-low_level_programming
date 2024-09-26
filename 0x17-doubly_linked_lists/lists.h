980f1df03f6054b7f9aea0d56ecbbf3f491bab04
abayomi olusanwo <absantec@gmail.com>
2024-09-26 15:04:03 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,15 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: points to previous node
- * @next: points to next node
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description: doubly linked list node struct
+ * Description: doubly linked list node structure
+ * for ALX project
  */
 typedef struct dlistint_s
 {
@@ -28,4 +30,5 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+
+#endif
